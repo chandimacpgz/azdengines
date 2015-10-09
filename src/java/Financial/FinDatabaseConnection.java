53080e889ccf11a31a38804d9eba6b2a75429917
Chandima <Chandima@MSI-CHAN>
2015-10-09 16:31:40 -0700

    EAD First Commit

---
+++ src/java/Financial/FinDatabaseConnection.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Financial;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Krish
+ */
+public class FinDatabaseConnection {
+
+
+    Connection conn;
+    Statement stmt;
+    ResultSet res;
+    public FinDatabaseConnection(){
+    
+    }
+    public Connection setConnection(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/engine","root","");
+        }catch(Exception e){
+            
+        }return conn;
+    }
+    public ResultSet getResult(String sql, Connection conn){
+        this.conn=conn;
+        try{
+            stmt=conn.createStatement();
+            res=stmt.executeQuery(sql);
+        }catch(Exception e){
+        
+        }return res;
+    }
+}
+  
+
+
